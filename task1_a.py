b15729cd5e4d629432f46fdbfb99b77a1debc9e8
Mohammed Hatem Monir <atmelshiekh@yahoo.com>
2025-08-01 12:24:03 +1000

    Created task 1 file with my name and ID.

---
+++ task1_a.py
@@ -0,0 +1,8 @@
+"""
+Group Name: Sydney 11
+Group Members:
+Mohamed Hatem Moneir Mansour Elshekh - 393891
+[Member 2 Name] - [Member 2 ID]
+[Member 3 Name] - [Member 3 ID]
+[Member 4 Name] - [Member 4 ID]
+"""
