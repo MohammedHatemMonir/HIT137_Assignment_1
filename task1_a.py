e12f71a8bef7eabe257a2efb8a70cc0390fbb8e8
Roshan Pandey <102504119+roshansir@users.noreply.github.com>
2025-08-02 17:03:19 +1000

    Update task1_a.py
    Added my name and ID

--- task1_a.py
+++ task1_a.py
@@ -2,7 +2,7 @@
 Group Name: Sydney 11
 Group Members:
 Mohamed Hatem Moneir Mansour Elshekh - 393891
-[Member 2 Name] - [Member 2 ID]
+Roshan Pandey - 395865
 [Member 3 Name] - [Member 3 ID]
 [Member 4 Name] - [Member 4 ID]
 """
