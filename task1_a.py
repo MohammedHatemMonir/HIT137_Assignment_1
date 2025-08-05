605ba8181f0b6722a5c7f6e156942950c3af6936
Mohammed Hatem Monir <atmelshiekh@yahoo.com>
2025-08-05 12:42:19 +1000

    about to handle float

--- task1_a.py
+++ task1_a.py
@@ -11,11 +11,18 @@ Kamana Thapa - 392322
 
 def can_form_triangle(a, b, c):
     return a + b > c and a + c > b and b + c > a
+
+
 def get_int_from_user(name): #function that takes input from user and CHECKS if it's int
     isNumber = False #Variable to keep track wether we have a number or not
     while isNumber == False: #This while loop keeps going until we have a number.
         try:
-            user_input = input(str(name))
+            user_input = input(str(name)) #write a custom message to the user
+            try:
+                val = float(user_input)
+            except:
+                pass
+
             val = int(user_input) #if this did not go to the except, we have got a valid number
             isNumber = True #mark that we got a number
             return val #Return the number and exit the while loop
@@ -29,7 +36,22 @@ y = get_int_from_user("Enter Second Number: ")
 z = get_int_from_user("Enter Third Number: ")
 
 if can_form_triangle(x, y, z):
-    print("Yes, these three lengths can form a triangle.")
+    print("Yes, %d, %d, %d can form a triangle." % (x,y,z))
 else:
-    print("NO, these three lengths CANNOT form a triangle.")
+    print("NO, %d, %d, %d CANNOT form a triangle." % (x,y,z))
 
+
+"""
+References:
+Stack overflow.
+https://stackoverflow.com 
+Used stack overflow for syntax correction and suggestions on how to check variable type
+
+w3schools
+https://www.w3schools.com/python
+Used w2schools for python syntax
+
+Brian McLogan (Youtube)
+https://youtu.be/gSmuLNGpf_E
+Used his video for the equation to determin if the numbers can form a triangle
+"""
